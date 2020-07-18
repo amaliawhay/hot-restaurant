89bc70e88c7e0d86698d4d8c9c21e603424c76fd
Amalia Hay <amaliawhay@gmail.com>
2020-07-18 13:01:32 -0700

    Removed errors displaying on page

--- script/server.js
+++ script/server.js
@@ -1,8 +1,7 @@
-HEAD
 //dependencies
 var express = require("express");
 var path = require("path");
-=======
+
 var express = require("express");
 var path = require("path");
 
@@ -15,4 +14,3 @@ app.use(express.json());
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-d1cea05ea870630d356cdf8b35ec1989a82bb328
