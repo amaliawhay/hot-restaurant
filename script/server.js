82ad6b0fd1df6f171f2c9dc3a79b8e67d1a037c7
Ian Martinez <imartinez0753@gmail.com>
2020-07-18 12:43:08 -0700

    adding routes

--- script/server.js
+++ script/server.js
@@ -7,6 +7,26 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//--------------------------------------------------------------
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/waitlist", function (req, res) {
+  res.sendFile(path.join(__dirname, "waitlist.html"));
+});
+
+//--------------------------------------------------------------
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
