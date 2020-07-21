f26b6a06efbd439ba3ec94b8dd4c6f279dde1ca6
Ian Martinez <imartinez0753@gmail.com>
2020-07-21 13:58:55 -0700

    clear button working

--- script/server.js
+++ script/server.js
@@ -9,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 const waitList = [];
-
+const tables = [];
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,15 +28,29 @@ app.get("/tables", function (req, res) {
 });
 
 app.get("/api/tables", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../html/tables.html"));
+  return res.json(tables);
+});
+//-------------------------------------------------------------------------------------------------
+app.post("/api/clear", function (req, res) {
+  var clearTable = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  // newTable.name = newTable.routeName.replace(/\s+/g, "").toLowerCase();
+
+  console.log(clearTable);
+
+  waitList.push(clearTable);
+
+  res.json(clearTable);
 });
 
-app.get("/api/clear", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../html/tables.html"));
-});
-
+// app.delete("/api/clear", function (req, res) {
+//   res.send("Got a DELETE request at /api/clear");
+// });
+//--------------------------------------------------------------------------------------------------
 app.get("/api/waitlist", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../html/waitlist.html"));
+  return res.json(waitList);
 });
 
 app.get("/api/tables/:table", function (req, res) {
@@ -44,9 +58,9 @@ app.get("/api/tables/:table", function (req, res) {
 
   console.log(chosen);
 
-  for (var i = 0; i < table.length; i++) {
-    if (chosen === table[i].routeName) {
-      return res.json(table[i]);
+  for (var i = 0; i < waitList.length; i++) {
+    if (chosen === waitList[i].costumerId) {
+      return res.json(waitList[i]);
     }
   }
 
